2937086da0e1d99e732a8f7ba085366eb2f8b38b
Oscar Herrera <a.oscarherrera@gmail.com>
2024-02-29 15:44:41 -0600

    product component modification

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 
 
+
 function Product({name,img}) {
+    const arrow = '../../public/audio-store-assets/shared/desktop/icon-arrow-right.svg'
     return (
         <div className="container-product">
             <div className="product">
                 <img src={img} alt="an image of the product" />
                 <h6>{name}</h6>
-                <p>Shop</p>
+                <p>Shop
+                    <img src={arrow}/>
+                </p>
             </div>
         </div>
 
