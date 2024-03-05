ec549adf10f07a9f33a92d9d0a942f2aa791a796
Oscar Herrera <a.oscarherrera@gmail.com>
2024-03-05 10:14:31 -0600

    footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,35 @@
+import '../sass/footer.scss'
+import Logo from '../../public/audio-store-assets/shared/desktop/logo.svg'
+
+
+
+function Footer() {
+    const socials = {
+        facebook: 'public/audio-store-assets/shared/desktop/icon-facebook.svg',
+        twitter: 'public/audio-store-assets/shared/desktop/icon-twitter.svg',
+        instagram: 'public/audio-store-assets/shared/desktop/icon-instagram.svg'
+    }
+    return (
+        <footer>
+            <div className='footer-content'>
+                <img src={Logo} alt="" />
+                <p>Audiophile is an all in one stop to fulfill your audio needs. We&#x60;re a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p>
+                <p>Copyright 2021. All Rights Reserved</p>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Headphones</li>
+                <li>Speakers</li>
+                <li>Earphones</li>
+                <div className='block-socials'>
+                    <img src={socials.facebook} alt="facebook icon" />
+                    <img src={socials.twitter} alt="twitter icon" />
+                    <img src={socials.instagram} alt="instagram icon" />
+                </div>
+            </ul>
+        </footer>
+    )
+}
+
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import MainSpeaker from './components/mainSpeaker'
 import SecondarySpeaker from './components/SecondarySpeaker'
 import Earphone from './components/EarPhone'
 import ProductBanner from './components/productBanner'
+import Footer from './components/footer'
 
 
 
@@ -18,5 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <SecondarySpeaker/>
     <Earphone/>
     <ProductBanner/>
+    <Footer/>
   </React.StrictMode>
 )
