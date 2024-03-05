4344fad3f67cb6b1755d885077d7b94dd1f95f61
Oscar Herrera <a.oscarherrera@gmail.com>
2024-03-05 09:19:24 -0600

    product banner relative path update

--- src/components/productBanner.jsx
+++ src/components/productBanner.jsx
@@ -10,7 +10,7 @@ function ProductBanner() {
                     <h2>Bringing you the <span>best</span> audiogear</h2>
                     <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
                 </div>
-                <img src="../../public/audio-store-assets/shared/desktop/image-best-gear.jpg" alt="" />
+                <img src="../../audio-store-assets/shared/desktop/image-best-gear.jpg" alt="" />
             </div>
         </>
     )
