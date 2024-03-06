84347caeab3deab29c2edff591468b63c7654f68
Oscar Herrera <a.oscarherrera@gmail.com>
2024-03-05 19:05:52 -0600

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,8 +10,8 @@ export default function HomePage() {
     return (
         <>
             <Hero/>
-            <MainSpeaker/>
             <ProductSection/>
+            <MainSpeaker/>
             <SecondarySpeaker/>
             <Earphone/>
             <ProductBanner/>
