5e1ba11a2c5012ffd87480bcc8a1808105c20ef3
Oscar Herrera <a.oscarherrera@gmail.com>
2024-03-07 11:43:49 -0600

    scroll to top component on router

---
+++ src/app.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage'
+import HeadphonesPage from './pages/HeadphonesPage'
+import SpeakersPage from './pages/SpeakersPage'
+import EarphonesPage from './pages/EarphonesPage'
+import ProductDetailsPage from './pages/ProductDetailsPage'
+import MarkOnePage from './pages/MarkIPage'
+import WhiteHeadphones from './pages/WhiteHeadphonesPage'
+import ZX9Speaker from './pages/ZX9SpeakerPage'
+import ZX7Speaker from './pages/ZX7SpeakerPage'
+import EarphoneDetails from './pages/EarphonesDetailsPage'
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage/>}></Route>
+            <Route path="/headphones" element={<HeadphonesPage/>}></Route>
+            <Route path="/speakers" element={<SpeakersPage/>}></Route>
+            <Route path="/earphones" element={<EarphonesPage/>}></Route>
+            <Route path="product-details-mark-two" element={<ProductDetailsPage/>}></Route>
+            <Route path="/product-details-mark-one" element={<MarkOnePage/>}></Route>
+            <Route path="/product-details-xx59" element={<WhiteHeadphones/>}></Route>
+            <Route path="/product-zx9-speaker" element={<ZX9Speaker/>}></Route>
+            <Route path="/product-zx7-speaker" element={<ZX7Speaker/>}></Route>
+            <Route path="/product-earphone" element={<EarphoneDetails/>}></Route>
+        </Routes>
+    )
+}
+
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,65 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
+import App from './app'
 import './sass/index.scss'
 
-import HomePage from './pages/HomePage'
-import HeadphonesPage from './pages/HeadphonesPage'
-import SpeakersPage from './pages/SpeakersPage'
-import EarphonesPage from './pages/EarphonesPage'
-import ProductDetailsPage from './pages/ProductDetailsPage'
-import MarkOnePage from './pages/MarkIPage'
-import WhiteHeadphones from './pages/WhiteHeadphonesPage'
-import ZX9Speaker from './pages/ZX9SpeakerPage'
-import ZX7Speaker from './pages/ZX7SpeakerPage'
-import EarphoneDetails from './pages/EarphonesDetailsPage'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/headphones',
-    element: <HeadphonesPage/>
-  },
-  {
-    path: '/speakers',
-    element: <SpeakersPage/>
-  },
-  {
-    path: '/earphones',
-    element:  <EarphonesPage/>
-  },
-  {
-    path: '/product-details-mark-two',
-    element: <ProductDetailsPage/>
-  },
-  {
-    path: '/product-details-mark-one',
-    element: <MarkOnePage/>
-  },
-  {
-    path: '/product-details-xx59',
-    element: <WhiteHeadphones/>
-  },
-  {
-    path: '/product-zx9-speaker',
-    element: <ZX9Speaker/>
-  },
-  {
-    path: '/product-zx7-speaker',
-    element: <ZX7Speaker/>
-  },
-  {
-    path: '/product-earphones',
-    element: <EarphoneDetails/>
-  }
-])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <ScrollToTop/>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 )
+
