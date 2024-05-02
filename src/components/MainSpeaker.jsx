8d94087a42ab8e97b901542beaf6a81458e1f25e
Oscar Herrera <a.oscarherrera@gmail.com>
2024-05-02 12:55:13 -0500

    main speaker component responsive

--- src/components/MainSpeaker.jsx
+++ src/components/MainSpeaker.jsx
@@ -1,7 +1,7 @@
 import '../sass/ProductSection.scss'
 import Button from "./Button";
 import { Link } from 'react-router-dom';
-
+import TabImg from '../../public/audio-store-assets/home/tablet/image-speaker-zx9.png';
 function MainSpeaker() {
 
 
@@ -11,9 +11,13 @@ function MainSpeaker() {
     }
 
     return (
-        <section>
+        <section className='section__main-speaker'>
             <article className='main-speaker-component'>
-                <img src={images.speaker} alt="an image of a speaker" />
+                <img 
+                    srcSet={`${TabImg} 900w, ${images.speaker}`}
+                    src={images.speaker} 
+                    alt="an image of a speaker" 
+                />
                 <div>
                     <h1>ZX9 SPEAKER</h1>
                     <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sounds.</p>
